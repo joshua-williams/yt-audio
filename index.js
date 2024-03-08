e9aed3d1cde817587f6a873ca9ebef32f26496a0
Joshua Williams <com.joshua360@gmail.com>
2024-03-07 22:53:23 -0600

    update storage directory name

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+const path = require('path');
 const { Command } = require('commander');
 const {exec} = require('child_process');
 const program = new Command();
@@ -7,11 +9,14 @@ program
   .argument('<url>', 'Youtube video url')
   .option('-f, --format <format>', 'the audio format to convert to', 'wav')
   .action((url, options) => {
-    const command = `./bin/yt-dlp ${url} -x --audio-format ${options.format} -o "storage/%(title)s.%(ext)s"`
+    const audioPath = path.join(__dirname, 'yt-audio')
+    const binPath = path.join(__dirname, 'bin/yt-dlp')
+    const command = `${binPath} ${url} -x --audio-format ${options.format} -o "${audioPath}/%(title)s.%(ext)s"`
     exec(command, (err, stdout, stderr) => {
       if (err) return console.log(stderr);
-      let [,fileName] = stdout.match(/\[ExtractAudio\] Destination: (storage\/.*)/);
-      exec(`open ./storage/`)
+      console.log(stdout)
+      const [, fileName] = stdout.match(/\[ExtractAudio\] Destination: (.*)/);
+      exec(`open ${path.join(__dirname, 'yt-audio')}`)
     });
   })
   .parse();
